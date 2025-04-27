76c44921daf50e707824a0b7f3ba818fb22163b5
Liam <liam@nehoray.net>
2025-04-27 21:33:42 +0200

    React 18 downgrade

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-svh">
-        <a href="/exam-info" ><Button className='hover:cursor-pointer' variant="outline">Maak een study plan</Button></a>
+        <a href="/exam-info" ><Button className='hover:cursor-pointer' variant="outline">Make a study plan</Button></a>
       </div>
     </>
   )
